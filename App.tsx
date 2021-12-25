bdb4343b8773bf21250f267d3d2bd08d12928258
kendi <og41168@ubt-uni.net>
2021-12-25 19:14:33 +0000

    added splash screen

--- App.tsx
+++ App.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import { SafeAreaView, StatusBar, Text, useColorScheme } from 'react-native';
+import RNBootSplash from 'react-native-bootsplash';
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
+  React.useEffect(() => {
+    const init = async () => {
+      // …do multiple sync or async tasks
+    };
+
+    init().finally(async () => {
+      await RNBootSplash.hide({ fade: true });
+      console.log('Bootsplash has been hidden successfully');
+    });
+  }, []);
+
   return (
     <SafeAreaView>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} backgroundColor={'transparent'} />
       <Text>dsds</Text>
     </SafeAreaView>
   );
--- android/app/src/main/java/com/habitbuilder/MainActivity.java
+++ android/app/src/main/java/com/habitbuilder/MainActivity.java
@@ -1,5 +1,7 @@
 package com.habitbuilder;
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate; // <- add this necessary import
+import com.zoontek.rnbootsplash.RNBootSplash; // <- add this necessary import
 
 public class MainActivity extends ReactActivity {
 
@@ -10,5 +12,17 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "HabitBuilder";
+  }
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+
+      @Override
+      protected void loadApp(String appKey) {
+        RNBootSplash.init(MainActivity.this); // <- initialize the splash screen
+        super.loadApp(appKey);
+      }
+    };
   }
 }
