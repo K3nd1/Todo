20df1512d7f545792dac3ad7526590bfb1ee2dd5
kendi <og41168@ubt-uni.net>
2022-01-01 22:25:14 +0000

    added eslint, prettier, huksy and lint-staged config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,20 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  extends: [
+    '@react-native-community',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
   rules: {
-    'no-shadow': 'off',
-    '@typescript-eslint/no-shadow': ['error'],
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    quotes: ['error', 'single'],
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error', { variables: false }],
+    indent: ['error', 2, { SwitchCase: 1 }],
+    'linebreak-style': ['error', 'windows'],
+    semi: ['error', 'always'],
+    '@typescript-eslint/explicit-module-boundary-types': ['error'],
+    '@typescript-eslint/no-explicit-any': ['error'],
   },
 };
--- App.tsx
+++ App.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { SafeAreaView, StatusBar, Text, useColorScheme } from 'react-native';
 import RNBootSplash from 'react-native-bootsplash';
 
-const App = () => {
+const App: React.FC = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
   React.useEffect(() => {
     const init = async () => {
       // …do multiple sync or async tasks
+      console.log('herree finished');
     };
 
     init().finally(async () => {
